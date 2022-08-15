9b7c2e7aa401ce7c01e9ead885ba71324c62af5d
Jaakko Heusala <jhh@sendanor.com>
2022-08-15 14:21:24 +0300

    Create notify-about-mail-in-spam.js

---
+++ notify-about-mail-in-spam.js
@@ -0,0 +1,46 @@
+/** Send spam summaries to another user. Version 1.0.0 / 2017-01-21 */
+function sendSpamSummary() {
+  var target_email = "jhh@sendanor.com";
+  var active_email = Session.getActiveUser().getEmail();
+ 
+  if (target_email === active_email) {
+    Logger.log("This is already target account.");
+    return
+  }
+  
+  Logger.log("This is user: " + active_email);
+  
+  var count = GmailApp.getSpamUnreadCount();
+  if (count === 0) {
+    Logger.log("No spam.");
+    return;
+  }
+  
+  var label = GmailApp.getUserLabelByName("spam-notified");
+  if (!label) {
+    label = GmailApp.createLabel("spam-notified");
+  }
+  
+  var subjects = [];
+  var threads = GmailApp.getSpamThreads();
+  threads.forEach(function(th) {
+    if ( th.getLabels().map(function(l) { return l.getName().toLowerCase(); }).indexOf('spam-notified') < 0) {
+      subjects.push( "  * " + th.getFirstMessageSubject());
+      th.addLabel(label);
+    }
+  });
+
+  if (subjects.length === 0) {
+    Logger.log("No new spam.");
+    return;
+  }
+  
+  var body = "You have new unread SPAM:\n\n";
+  body += subjects.join('\n');
+  body += "\n";
+  body += "-- \n";
+  body += "Sendanor's Google App Script\n";
+  
+  GmailApp.sendEmail(target_email, "New spam in " + active_email, body);
+  Logger.log("Sent summary to " + target_email);
+}
